6d2a1b0189987539db688bd6bf82cbfd7a57c25e
SNOWIK1 <dimais444@mail.ru>
2023-09-06 11:47:52 +0700

    half of...

---
+++ WPF-laba-26/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_laba_26
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : NavigationWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            (sender as FrameworkElement).Style = (Style)Resources["tb_search_no"];
+        }
+
+        private void TB_LostFocus(object sender, RoutedEventArgs e)
+        {
+            (sender as FrameworkElement).Style = (Style)Resources["tb_search"];
+        }
+    }
+}
